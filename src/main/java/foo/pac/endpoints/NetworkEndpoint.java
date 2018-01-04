7698fe7f02270c7b9a7e0e80fecab849fb7e8f51
Brad Kawaii <avator888@gmail.com>
2018-01-04 00:11:29 -0500

    add telnet command

--- src/main/java/foo/pac/endpoints/NetworkEndpoint.java
+++ src/main/java/foo/pac/endpoints/NetworkEndpoint.java
@@ -16,8 +16,7 @@ import javax.ws.rs.core.Response;
 public class NetworkEndpoint {
 
     /**
-     * good old ping
-     * http://localhost:8080/rad/ping/127.0.0.1
+     * good old ping http://localhost:8080/rad/ping/127.0.0.1
      *
      */
     @GET
@@ -72,6 +71,47 @@ public class NetworkEndpoint {
             while ((inputLine = in.readLine()) != null) {
                 result.append("<br/>");
                 result.append(inputLine);
+            }
+            in.close();
+
+        } catch (IOException ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ErrorPayload(ex.getMessage())).build();
+        }
+
+        // plain text - console like
+        Response response = Response.ok(result.toString(), MediaType.TEXT_HTML).build();
+        return response;
+    }
+
+    /**
+     * telnet as well; main purpose to check that port on remote server is open
+     * http://localhost:8080/rad/curl?url=https://www.theglobeandmail.com
+     */
+    @GET
+    @Path("/telnet/{ip}/{port}")
+    @Produces({MediaType.TEXT_HTML})
+    public Response telnet(@PathParam("ip") String ip, @PathParam("port") String port) {
+
+        // prepare payload
+        StringBuilder result = new StringBuilder();
+
+        String cmd = "telnet " + ip + " " + port;
+        try {
+            Runtime r = Runtime.getRuntime();
+            Process p = r.exec(cmd);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String inputLine;
+            int i = 0;
+            while ((inputLine = in.readLine()) != null) {
+                result.append("<br/>");
+                result.append(inputLine);
+
+                // just in case
+                i++;
+                if (i > 100) {
+                    break;
+                }
             }
             in.close();
 
